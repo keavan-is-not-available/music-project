64af896682543999924a59a291270b34ac5ba373
keavan-is-not-available <116943936+keavan-is-not-available@users.noreply.github.com>
2024-04-13 09:50:12 +0800

    Add files via upload

--- smth.js
+++ smth.js
@@ -1,3 +1,9 @@
+music1 = ""
+music2 = ""
+function preload(){
+ music1 = loadSound("music.mp3")
+ music2 = loadSound("music2.mp3")
+}
 function setup() {
     canvas = createCanvas(400, 300)
     vid = createCapture(VIDEO)
@@ -6,3 +12,4 @@ function setup() {
 function draw() {
     image(vid, 0, 0, 400, 300)
 }
+
